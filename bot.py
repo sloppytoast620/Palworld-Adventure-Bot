1c971f4980b95e86cd5d878888912968cab767b4
sloppytoast620 <sloppytoast620@gmail.com>
2026-02-04 15:15:55 -0500

    Initialize Discord bot with commands

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+# Import modules
+from characters import create_character_command, character_info_command
+from pals import explore_command
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Register commands
+bot.add_command(create_character_command)
+bot.add_command(character_info_command)
+bot.add_command(explore_command)
+
+# Run the bot
+bot.run("YOUR_BOT_TOKEN")
