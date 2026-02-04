ef88c9bea67f1d69ff100ed9727eb33f1c7f96f2
sloppytoast620 <sloppytoast620@gmail.com>
2026-02-04 15:59:40 -0500

    Enable member intents and log bot readiness
    Added member intents and on_ready event for bot.

--- bot.py
+++ bot.py
@@ -9,7 +9,13 @@ from pals import explore_command
 intents = discord.Intents.default()
 intents.message_content = True
 
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.tree.sync()
 
 # Register commands
 bot.add_command(create_character_command)
