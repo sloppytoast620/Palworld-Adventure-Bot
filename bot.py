560f416107905e18a95ad73ec1d2de50087563e5
sloppytoast620 <sloppytoast620@gmail.com>
2026-02-10 10:26:15 -0500

    Fix import statement for explore_command

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 # Import modules
 from characters import create_character_command, character_info_command
-from pals_commands import explore_command
+from pal_commands import explore_command
 
 intents = discord.Intents.default()
 intents.message_content = True
